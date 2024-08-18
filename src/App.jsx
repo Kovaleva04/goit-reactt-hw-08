76a89469b568b0b9e9577135409b4b8897d3964a
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-08-18 16:28:34 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { selectIsRefreshing } from "./redux/auth/selectors";
 import ContactsPage from "./pages/ContactsPage/ContactPage";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const RegisterPage = lazy(() => import("./pages/RegistrationPage/RegistrationPage"));
+const RegistrationPage = lazy(() => import("./pages/RegistrationPage/RegistrationPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 
 export const App = () => {
